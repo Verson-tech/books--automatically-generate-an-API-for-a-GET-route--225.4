8f86c2a1a16299d7ee0f50794affe5dd3d10e675
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-10-15 15:00:39 +0300

    post route

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require ('express')
-const app =  express();
+const express = require('express');
+const app     = express();
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 
+// data parser - used to parse post data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const swaggerOptions = {
-    swaggerDefinition:{
-        info : {
+    swaggerDefinition: {
+        info: {
             title: 'Library API',
             version: '1.0.0'
         }
@@ -25,18 +30,38 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 *         description: Success
 *
 */
+app.get('/books', (req,res) => {
+    res.send([
+        {
+            isbn : '9781781100486',
+            title: 'Harry Potter and the Sorcerer\'s Stone',
+            author: 'J.K. Rowling',
+            publisher: 'Scholastic'
+        }
+    ]);
+});
 
+/**
+* @swagger
+* /book:
+*   post:
+*     description: Get one book
+*     parameters:
+*     - name: title
+*       description: Book title
+*       in: body
+*       required: true
+*       type: string
+*     responses:
+*       200:
+*         description: Success
+*
+*/
+app.post('/book', (req,res) => {
+    const title = req.body.title;
+    res.send({ title});
+});
 
-app.get('/books',(req,res)=>{
-    res.send([{
-        isbn : '9781781100486',
-        title: 'Harry Potter',
-        author: 'J.K. Rowling',
-        publisher: 'Scholastic'
-    }
-]);
-})
-
-app.listen(3000,() => {
-    console.log('Running on port 3000!');
+app.listen(3000, () => {
+    console.log('Running on port 3000');
 })
